2095b7cdd070055932d0cf304b3a6cb26036a0e3
wsomdev <56587373+wsomdev@users.noreply.github.com>
2026-04-14 21:15:37 +0200

    Fix interface

--- src/app/models/enemy.interface.ts
+++ src/app/models/enemy.interface.ts
@@ -1,7 +1,10 @@
 import { EnemyRaceType } from '../enums/enemy-race-type.enum';
 import { IBaseEntity, IBaseInstance } from './base-entity.interface';
 
-export interface IEnemy extends IBaseEntity, IBaseInstance {
-  kind: 'normal' | 'elite' | 'boss';
+export interface IEnemy extends IBaseEntity {
   race: EnemyRaceType;
+}
+
+export interface IEnemyInstance extends IEnemy, IBaseInstance {
+  kind: 'normal' | 'elite' | 'boss';
 }
